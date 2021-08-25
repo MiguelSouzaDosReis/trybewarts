0091beaaa0c53174f6e46f773d033c8a402f2320
Ericksonn Siqueira <ericksonsiqueirasilva@gmail.com>
2021-08-25 17:36:37 -0300

    Criando radios de avaliaçao

--- script.js
+++ script.js
@@ -9,3 +9,23 @@ loginButton.addEventListener('click', () => {
     window.alert('Email ou senha inválidos.');
   }
 });
+
+function creatRateOptions() {
+  const avaliacao = document.getElementById('rate')
+  for (let index = 1; index <= 10; index++) {
+    let label = document.createElement('label');
+    label.for = index;
+    
+    let opicoes = document.createElement('input');
+    opicoes.id = index
+    opicoes.type = 'radio';
+    opicoes.value = index;
+    opicoes.name = 'rate';
+    label.appendChild(opicoes);
+
+    label.innerHTML += index;
+    
+    avaliacao.appendChild(label);
+  }
+}
+creatRateOptions()
